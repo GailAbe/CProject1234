95ed81359c370a312f83764a8d0f228ea7c61c3e
Abegail Tambal <tambalabegail@gmail.com>
2022-10-01 11:13:47 +0800

    Change providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(125);
+        Paginator::useBootstrap();
     }
 }
