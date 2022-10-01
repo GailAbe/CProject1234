f5a13702712fcacd615f00fade9038ad4ed11bc6
Abegail Tambal <tambalabegail@gmail.com>
2022-10-01 17:59:59 +0800

    refactor authentication

---
+++ app/Http/Controllers/HouseholdController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HouseholdController extends Controller
+{
+    public function index()
+    {
+        return view('');
+    }
+
+    public function create()
+    {
+        return view('');
+    }
+
+    public function store(Request $request)
+    {
+        return view();
+    }
+
+    public function update(Request $request, $id)
+    {
+        return view();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\Auth\AuthController;
 
 /*
@@ -15,19 +16,19 @@ use App\Http\Controllers\Auth\AuthController;
 */
 
 
-Route::get('/', function () {
-    return view('modules/dashboard');
+// Route::get('/', function () {
+//     return view('modules/dashboard');
+// });
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/store', [AuthController::class, 'validateUser'])->name('auth.store');
 });
 
-// Route::middleware('guest')->group(function () {
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/store', [AuthController::class, 'validateUser'])->name('auth.store');
-// });
-
-// Route::middleware('auth')->group(function () {
-//     // Route::get('/', [MainController::class, 'home'])->name('home');
-//     // Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-// });
+Route::middleware('auth')->group(function () {
+    Route::get('/', [MainController::class, 'home'])->name('home');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
 Route::controller(ComplaintController::class)->group(function () {
     Route::group([
