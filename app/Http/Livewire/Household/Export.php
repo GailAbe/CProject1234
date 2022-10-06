a9ce61923b35c8de95754c9785e02563b0351189
Abegail Tambal <tambalabegail@gmail.com>
2022-10-07 00:08:42 +0800

    Add export on household

--- app/Http/Livewire/Household/Export.php
+++ app/Http/Livewire/Household/Export.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire\Household;
 
 use Livewire\Component;
 use App\Models\Household;
+use App\Services\Constant;
+use PhpOffice\PhpWord\TemplateProcessor;
 
 class Export extends Component
 {
@@ -11,9 +13,43 @@ class Export extends Component
 
     public function export()
     {
-        $this->households = Household::with('members')->get();
+        $path = storage_path(Constant::TEMPLATE_PATH_HOUSEHOLD);
+        $templateProcessor = new TemplateProcessor($path);
+        $this->households->load('members');
 
         // dd($this->households);
+        $count = $this->households->count();
+
+        if ($count > 0) {
+            $templateProcessor->cloneRow('n', $count);
+            $i = 1;
+            foreach ($this->households as $key => $household) {
+                $templateProcessor->setValue('n#' . $i, $i);
+                $templateProcessor->setValue('h_number#' . ($key + 1), $household->household_number);
+                $templateProcessor->setValue('purok#' . ($key + 1), $household->purok_name);
+                $templateProcessor->setValue('fhead#' . ($key + 1), $household->fhead_name);
+                $templateProcessor->setValue('gend#' . ($key + 1), $household->fhead_gender);
+                $templateProcessor->setValue('bdate#' . ($key + 1), $household->fhead_bdate);
+                $templateProcessor->setValue('status#' . ($key + 1), $household->fhead_cstatus);
+                $i++;
+                // foreach ($household->members as $key => $member) {
+                //     $templateProcessor->setValue('mn#' . $i, $i);
+                //     $templateProcessor->setValue('mname#' . ($key + 1), $member->fullname);
+                //     $templateProcessor->setValue('mpurok#' . ($key + 1), $member->purok_name);
+                //     $templateProcessor->setValue('mgend#' . ($key + 1), $member->gender);
+                //     $templateProcessor->setValue('mbdate#' . ($key + 1), $member->bdate);
+                //     $templateProcessor->setValue('mstatus#' . ($key + 1), $member->cstatus);
+
+                //     $i++;
+                // }
+            }
+        }
+
+        $filename = 'household-' . date('Y-m-d-h-i-s-a');
+        $tempPath = 'reports/' . $filename . '.docx';
+
+        $templateProcessor->saveAs($tempPath);
+        return response()->download(public_path($tempPath));
     }
 
     public function render()
---
+++ app/Services/Constant.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services;
+
+class Constant
+{
+    public const TEMPLATE_PATH_HOUSEHOLD = 'docx/household_temp.docx';
+
+    public static function getPositions()
+    {
+        return [
+            'Brgy-Chairman',
+            'Brgy-Secretary',
+            'Brgy-Treasurer',
+            'Brgy-Kagawad',
+            'Sk-Chairperson',
+            'Sk-Secretary',
+            'Sk-Treasurer',
+            'Sk-Kagawad',
+        ];
+    }
+
+    public static function getOtherPositions()
+    {
+        return [
+            'Appropriation',
+            'BAC Chairman',
+            'Vice BAC Chairman',
+            'BMO',
+        ];
+    }
+
+
+    public static function months()
+    {
+        return [
+            'January',
+            'February',
+            'March',
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ];
+    }
+}
